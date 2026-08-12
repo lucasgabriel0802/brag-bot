259b29dbef3a04f7e6df6ed41f39f2053f485015
Lucas Gabriel dos Santos <lucasgabriel0802@gmail.com>
2026-08-11 21:47:01 -0300

    feat: setup do backend com model, rotas e fluxo do Genkit
    Criacao do modelo Brag, migration, BragController, rotas em web.php e configuracao do fluxo de IA usando Genkit em src/flows.ts e dependencias no package.json. Inclui as formatacoes feitas pelo Pint.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\BragController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [BragController::class, 'index'])->name('dashboard');
+Route::get('/brag/{id}', [BragController::class, 'show'])->name('brag.show');
+Route::post('/brag/generate', [BragController::class, 'generate'])->name('brag.generate');
